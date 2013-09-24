33e3f1618acbe466fec5df88873e8a4d6d820059
micahrye - skittles <micahrye@gmail.com>
2013-09-24 15:51:10 -0400

    REFACTOR: of download manager code and unzip code.

--- src/fslt/lib/file/UnzipService.java
+++ src/fslt/lib/file/UnzipService.java
@@ -4,27 +4,43 @@ import java.io.File;
 
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
-public class UnzipService extends Service {
+/**
+ * NOTE: when extending if you want to debug your extended service you will need 
+ * to make sure that android:process is not set. Setting 'process' gives the service
+ * its own process to run in. Default android:process=":fslt.lib.file.utilites.UnzipService"
+ * 
+ * @author affect
+ * @see http://developer.android.com/guide/components/processes-and-threads.html
+ * @see http://developer.android.com/guide/topics/manifest/service-element.html
+ */
+public abstract  class UnzipService extends Service {
 
 	private final static String TAG = UnzipService.class.getSimpleName();
+	public final static String FILE_NAME = "FILE_NAME"; 
+	public final static String UNZIP_FILE_TO_LOCATION = "UNZIP_LOCATION"; 
+	public final static String ZIP_FILE_LOCATOIN = "ZIP_FILE_LOACTION";
+	public final static String SHOW_TOAST = "SHOW_TOAST"; 
+	public final static String DELETE_ZIP_FILE_AFTER_UNZIP = "DELETE_ZIP_FILE_AFTER_DELET"; 
+	private boolean mShowToast; 
+	private boolean mDeleteZipFile; 
+	private Context mContext; 
 	
 	@Override
-	public IBinder onBind(Intent arg0) {
-		// TODO Auto-generated method stub
-		Log.e(TAG, "UNZIP!!!");
+	public IBinder onBind(Intent intent) {
 		return null;
 	}
 	
 	@Override
     public void onCreate() {
-		Log.e(TAG, "UNZIP!!!");
-        Toast.makeText(this, "fslt: unziping story", Toast.LENGTH_SHORT).show();
+		mContext = this.getApplicationContext(); 
+        Toast.makeText(mContext, "fslt: unziping story", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -34,54 +50,82 @@ public class UnzipService extends Service {
     	 * onStartCommand() is also called. Therefore your service needs to be 
     	 * prepared that onStartCommand() can be called several times.
     	 */
-    	Log.e(TAG, "UNZIP!!!");
-    	String storyName = intent.getStringExtra("STORY_NAME");
-    	String unzipLocation = intent.getStringExtra("UNZIP_LOCATION");
-    	String zipLocation = intent.getStringExtra("ZIP_FILE_LOCATION");
+    	String fileName = intent.getStringExtra(FILE_NAME);
+    	String zipFileLocation = intent.getStringExtra(ZIP_FILE_LOCATOIN);
+    	String unzipFileToLocation = intent.getStringExtra(UNZIP_FILE_TO_LOCATION);
+    	mShowToast = intent.getBooleanExtra(SHOW_TOAST, false);
+    	mDeleteZipFile = intent.getBooleanExtra(DELETE_ZIP_FILE_AFTER_UNZIP, true);
     	
-    	Toast.makeText(this, storyName + " unzipping :fslt: ", Toast.LENGTH_SHORT).show();
-    	new UnzipAStory().execute(zipLocation, unzipLocation.toString(), storyName);
-    	
+    	if(mShowToast){
+    		Toast.makeText(mContext, fileName + " unzipping :fslt: ", Toast.LENGTH_SHORT).show();
+    	}
+    	//start thread to unzip
+    	handleZipFile(zipFileLocation, unzipFileToLocation, fileName); 
     	return Service.START_NOT_STICKY;
     }
     
-    private class UnzipAStory extends AsyncTask<String, Integer, String[]> {
+    /**
+     * Implement your own method for handeling downloaded zip of use DefaultUnzipTask
+     * to complete the unzip. NOTE that you will want to run a thread to complete your
+     * action on.
+     * 
+     * @param zipFileLoaction
+     * @param unzipFileToLocation
+     * @param fileName
+     */
+    public abstract void handleZipFile(String zipFileLoaction, String unzipFileToLocation, String fileName); 
+    
+    private boolean deleteZipFile(String zipFileToDelete){
+    	File file = new File(zipFileToDelete);
+    	boolean deleted = file.delete();
+    	if(!deleted){
+    		if(mShowToast){
+    			Toast.makeText(mContext, "Problem delete zip file:  " + zipFileToDelete, 
+    					Toast.LENGTH_SHORT).show();
+    		}
+    	}
+    	return deleted;
+    }
+    /**
+     * 
+     * @author affect
+     *
+     */
+    public class DefaultUnzipFileTask extends AsyncTask<String, Integer, Boolean> {
         @Override
-		protected String[] doInBackground(String... locations)
+		protected Boolean doInBackground(String... locations)
         {
-        	Log.e(TAG, "UNZIP!!!");
         	// Delete the entire old folder if it existed
         	FileOperations.deleteRecursive(new File(locations[1])); 
-        	
+        	String fileToUnzip = locations[0]; 
+        	String locationToUnzipFile = locations[1]; 
             //Try to unzip the file
             try{
-            	Log.d(TAG, "start to decompress " + locations[0]);
-                Decompress d = new Decompress(locations[0], locations[1]);
-                d.unzip();
-                Log.d(TAG, locations[0] + " now unziped");
+            	Log.d(TAG, "start to decompress " + fileToUnzip);
+                Decompress fileToDecompress = new Decompress(fileToUnzip, locationToUnzipFile);
+                fileToDecompress.unzip();
+                if(mDeleteZipFile){
+                	deleteZipFile(fileToUnzip); 
+                }
             } catch (Exception e){
             	//Before we were looking for FileNotFoundExcption, but we where not catching or throwing that.
-            	// changed to Exception and this is now thrown from unzip
-            	Log.d(TAG, "error decompressing " + locations[0]);
+            	if(mShowToast){
+            		Toast.makeText(mContext, fileToUnzip + ": error unziping ", Toast.LENGTH_SHORT).show();
+            	}
+            	Log.d(TAG, "error decompressing " + fileToUnzip);
                 e.printStackTrace();
+                return false;
             }
             //return file location so that we can delete the original zip file. 
-            return locations;
+            return true;
         }
 
         @Override
-		protected void onPostExecute(String[] result)
+		protected void onPostExecute(Boolean result)
         {
-        	// IMPORTANT
-        	// Appears that you can get old zips that don't get deleted in 
-        	Log.e(TAG, "delete " + result[0]);
-        	File file = new File(result[0]);
-        	boolean deleted = file.delete();
-        	if( !deleted )
-        		Log.e(TAG, result[0] + " not deleted");
-            return;
+        	
         }
-    }; // End UnzipAStory
+    };
 
     
 }
--- src/fslt/lib/network/DownloadManagerReceiver.java
+++ src/fslt/lib/network/DownloadManagerReceiver.java
@@ -5,6 +5,7 @@
  */
 package fslt.lib.network;
 
+import java.io.File;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -37,139 +38,141 @@ import android.widget.Toast;
  * <p> 
  * In your code to download you will want to request the download from DownloadManager, and 
  * you may want to check to see if the file is already in the queue for download. 
- * <pre>
- * 	
- * 			private void someFunction(){			
- * 				Request request = new Request(Uri.parse(url));
-				request.allowScanningByMediaScanner();
-				request.setNotificationVisibility(
-						DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-
-				// actuall file name being downloaded
-				String filename = url.substring(url.lastIndexOf("/")+1, url.length());
-
-				//Check if file is currently being downloaded, if so do not download it. 
-				DownloadManager.Query query = new DownloadManager.Query();
-				query.setFilterByStatus(DownloadManager.STATUS_RUNNING | 
-							DownloadManager.STATUS_PENDING | DownloadManager.STATUS_PAUSED);
-				Cursor cursor = mDownloadMngr.query(query);
-				if(cursor.moveToFirst()){
-					int columnTitle = cursor.getColumnIndex(DownloadManager.COLUMN_TITLE);
-					String downloadFileTitle = cursor.getString(columnTitle);
-					boolean same = downloadFileTitle.equalsIgnoreCase(filename); 
-					same = downloadFileTitle.equalsIgnoreCase("omg_bees".toString()); 
-					if(downloadFileTitle.equalsIgnoreCase( filename )){
-						Toast.makeText(mCtx, "Alreading downloading file", 1000).show();
-						return;
-					}else{
-						requestDownload(request, filename);
-						Toast.makeText(mCtx, "Downloading.... :" + filename, Toast.LENGTH_LONG).show();
-					}
-				}else{
-					requestDownload(request, filename);
-					Toast.makeText(mCtx, "Downloading.... :" + filename, Toast.LENGTH_LONG).show();
-				}
-
-			}
-			
-	private void requestDownload(Request request, String filename){
-		
-		request.setDestinationInExternalFilesDir(mCtx, Environment.DIRECTORY_DOWNLOADS, filename);
-		request.setDescription("STORYSCAPE_STORY_DOWNLOAD");
-		request.setTitle(filename);
-		enqueue = mDownloadMngr.enqueue(request);
-		
-	}
- * </pre> 
+ * 
  */
 public abstract  class DownloadManagerReceiver extends BroadcastReceiver {
 	private final static String TAG = DownloadManagerReceiver.class.getSimpleName(); 
 	public String mPathInfo = null; 
 	//TODO: since this is a broadcast receiver that  
-	protected String mRootAppStorageDir = "StoryScape"; 
-	
+	protected String mRootAppStorageDir = ""; 
+
 	protected abstract void setRootApplicationStorageDirectory();	
-	
+
 	@Override
-	public void onReceive(Context ctx, Intent intent) {
+	public void onReceive(Context context, Intent intent) {
 		setRootApplicationStorageDirectory(); 
-		
+
 		String action = intent.getAction();
-        if (DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(action)) {
-            long downloadId = intent.getLongExtra(
-                    DownloadManager.EXTRA_DOWNLOAD_ID, 0);
-            Query query = new Query();
-            query.setFilterById(downloadId);
-            DownloadManager mDownloadMngr = 
-            		(DownloadManager)ctx.getSystemService(Context.DOWNLOAD_SERVICE);
-             
-            Cursor c = mDownloadMngr.query(query);
-            if (c.moveToFirst()) {
-                int columnIndex = c
-                        .getColumnIndex(DownloadManager.COLUMN_STATUS);
-                if (DownloadManager.STATUS_SUCCESSFUL == c.getInt(columnIndex) ) {
-                    String zipUri = c
-                            .getString(c
-                                    .getColumnIndex(DownloadManager.COLUMN_LOCAL_URI))
-                            .substring(7);
-                    Log.e(TAG, "DID" + downloadId + " downloaded " + zipUri);
-                    String storyURI = zipUri.replaceAll("-\\d\\.zip\\z", ".zip"); 
-                    Pattern pattern = Pattern.compile(".*\\.zip\\z");
-                    Matcher matcher = pattern.matcher(storyURI);
-                    /*
-                     IF we have a zip file continue. THIS should be changed, need to make this an
-                     abstract class so user can implement whatever they want for when the file 
-                     they downloaded is done. 
-                    */
-                    if(matcher.find()){
-                    	unpackageZip(ctx, matcher, zipUri); 
-                    }
-                }else{
-                	Log.d(TAG, "not success"); 
-                }
-            }else{
-            	Log.d(TAG, "not complete"); 
-            }
-        }
-    }
-	
-	private void unpackageZip(Context ctx, Matcher matcher, String zipUri){
-		String storyName = "";
-		try{
-    		int start = matcher.group().lastIndexOf("/") + 1;
-    		int end = matcher.group().length()-4; 	
-    		storyName = matcher.group().substring(start, end);
-    		/* Now, if it's a zip file,  we want to unzip it...
-             * It is possible to be download a story more than once, which results in 
-             * the story name being appended with '-2', i.e. dash number. We want to remove 
-             * that so that our unpacked folder name is only the story name. Otherwise, we 
-             * have the same story in our device library with the appended dash number, even
-             * though they are all the same story. 
-             */
-    		
-    	}catch( IllegalStateException e ){
-    		//Should raise an error 
-    		e.printStackTrace(); 
-    	}
-        StringBuilder unzipLocation = new StringBuilder();
-        /* IMPORTANT: MRE
-         * need to add story directory name and unzip into that and then remove zip file. 
-         * zip is crated 'zip -rj' and opened 'unzip storyname.zip -d storyname/
-         */
-        String pathInfo = "/" + mRootAppStorageDir  + "/" + storyName + "/"; 
-        //Currently unziping to SDCARD
-        //TODO: give ability to change location, may want internal storage location or other. 
-        unzipLocation.append(Environment.getExternalStorageDirectory() + pathInfo);  
-        //String loc = unzipLocation.toString(); 
-        Log.e(TAG, "call unzip for " + pathInfo);
-        
-        //new UnzipAStory().execute(zipUri, unzipLocation.toString(), storyName);
-        
-        Intent unzipIntent = new Intent(ctx, UnzipService.class);
-        unzipIntent.putExtra("STORY_NAME", storyName);
-        unzipIntent.putExtra("UNZIP_LOCATION", unzipLocation.toString() );
-        unzipIntent.putExtra("ZIP_FILE_LOCATION", zipUri);
-        ctx.startService(unzipIntent);
+		if (DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(action)) {
+			long downloadId = intent.getLongExtra(
+					DownloadManager.EXTRA_DOWNLOAD_ID, 0);
+			Query query = new Query();
+			query.setFilterById(downloadId);
+			DownloadManager mDownloadMngr = 
+					(DownloadManager)context.getSystemService(Context.DOWNLOAD_SERVICE);
+
+			Cursor c = mDownloadMngr.query(query);
+			if (c.moveToFirst()) {
+				int columnIndex = c
+						.getColumnIndex(DownloadManager.COLUMN_STATUS);
+				if (DownloadManager.STATUS_SUCCESSFUL == c.getInt(columnIndex) ) {
+					String downloadedFileUri = c
+							.getString(c
+									.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI))
+									.substring(7);
+
+					handleDownloadedFile(context, downloadedFileUri);
+
+				}else{
+					Log.d(TAG, "not success"); 
+				}
+			}else{
+				Log.d(TAG, "not complete"); 
+			}
+		}
 	}
-	
+
+	private DownloadManager mDownloadMngr;
+
+	public long requestDownloadFile(Context context, String url, String fileName){
+
+		mDownloadMngr = (DownloadManager) context.getSystemService(context.DOWNLOAD_SERVICE);
+
+		Request request = new Request(Uri.parse(url));
+		request.allowScanningByMediaScanner();
+		request.setNotificationVisibility(
+				DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+
+		//String fileName = url.substring(URL.lastIndexOf("/")+1, url.length());
+
+		//Check if file is currently being downloaded, if so do not download it. 
+		DownloadManager.Query query = new DownloadManager.Query();
+		query.setFilterByStatus(DownloadManager.STATUS_RUNNING | 
+				DownloadManager.STATUS_PENDING | DownloadManager.STATUS_PAUSED);
+		Cursor cursor = mDownloadMngr.query(query);
+		if(cursor.moveToFirst()){
+			int columnTitle = cursor.getColumnIndex(DownloadManager.COLUMN_TITLE);
+			String downloadFileTitle = cursor.getString(columnTitle);
+			boolean same = downloadFileTitle.equalsIgnoreCase(fileName); 
+			same = downloadFileTitle.equalsIgnoreCase("omg_bees".toString()); 
+			if(downloadFileTitle.equalsIgnoreCase( fileName )){
+				Toast.makeText(context, "Alreading downloading file", 1000).show();
+				return -1l;
+			}else{
+				return requestDownload(context, request, fileName);
+			}
+		}else{
+			return requestDownload(context, request, fileName);
+		}
+
+	}
+
+	private long requestDownload(Context context, Request request, String filename){
+		request.setDestinationInExternalFilesDir(context, Environment.DIRECTORY_DOWNLOADS, filename);
+		request.setDescription("STORYSCAPE_STORY_DOWNLOAD");
+		request.setTitle(filename);
+		return mDownloadMngr.enqueue(request);
+	}
+	/**
+	 * 
+	 * @param context
+	 * @param downloadedFileUri
+	 */
+	public abstract void handleDownloadedFile(Context context, String downloadedFileUri); 
+
+	/**
+	 * 
+	 * @param context
+	 * @param downloadedFileUri
+	 * @param unzipServiceIntent
+	 */
+	public void defaultUnzipFile(Context context, String downloadedFileUri, Intent unzipServiceIntent){
+		String fileName = "";
+		String storyURI = downloadedFileUri.replaceAll("-\\d\\.zip\\z", ".zip"); 
+		String storyName = "";
+		Pattern pattern = Pattern.compile(".*\\.zip\\z");
+		Matcher matcher = pattern.matcher(storyURI);
+		if(matcher.find()){
+			try{
+				int start = matcher.group().lastIndexOf("/") + 1;
+				int end = matcher.group().length()-4; 	
+				fileName = matcher.group().substring(start, end);
+				/* Now, if it's a zip file,  we want to unzip it...
+				 * It is possible to be download a story more than once, which results in 
+				 * the story name being appended with '-2', i.e. dash number. We want to remove 
+				 * that so that our unpacked folder name is only the story name. Otherwise, we 
+				 * have the same story in our device library with the appended dash number, even
+				 * though they are all the same story. 
+				 */
+			}catch( IllegalStateException e ){
+				//Should raise an error 
+				e.printStackTrace(); 
+			}
+			// zip is crated 'zip -rj' and opened 'unzip fileName.zip -d fileName/
+			StringBuilder unzipLocation = new StringBuilder();
+			unzipLocation.append(Environment.getExternalStorageDirectory());
+			unzipLocation.append(File.separator).append(mRootAppStorageDir); 
+			unzipLocation.append(File.separator).append(fileName).append(File.separator);
+
+			unzipServiceIntent.putExtra(UnzipService.FILE_NAME, fileName);
+			unzipServiceIntent.putExtra(UnzipService.ZIP_FILE_LOCATOIN, downloadedFileUri);
+			unzipServiceIntent.putExtra(UnzipService.UNZIP_FILE_TO_LOCATION, unzipLocation.toString() ); 
+			unzipServiceIntent.putExtra(UnzipService.SHOW_TOAST, true); 
+			unzipServiceIntent.putExtra(UnzipService.DELETE_ZIP_FILE_AFTER_UNZIP, true);
+			context.startService(unzipServiceIntent);
+		}else{
+			Log.d(TAG, "something about file name wacky"); 
+		}
+	}
+
 }
