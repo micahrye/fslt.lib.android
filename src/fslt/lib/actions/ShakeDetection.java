2c33f6cc9516f092797e62feb65978acf98f4c99
Craig Ferguson <craig.ferguson@babbaco.com>
2013-10-09 18:04:33 -0400

    making the threshold app-dependent

--- src/fslt/lib/actions/ShakeDetection.java
+++ src/fslt/lib/actions/ShakeDetection.java
@@ -15,29 +15,37 @@ public class ShakeDetection implements SensorEventListener {
 	private float mAccel; // acceleration apart from gravity
 	private float mAccelCurrent; // current acceleration including gravity
 	private float mAccelLast; // last acceleration including gravity
+	private final float mThreshold;
+
 	private static final String SHAKEN = "SHAKEN";
 	private static final String SHAKE_STRENGTH = "SHAKE_STRENGTH";
-	private static final float SHAKE_THRESHOLD = 7.0f;
+	private static final float SHAKE_THRESHOLD_DEFAULT = 7.0f;
 
 	private final Context mCtx;
-	
-	public ShakeDetection(Context c){
+
+	public ShakeDetection(Context c) {
+		this(c, SHAKE_THRESHOLD_DEFAULT);
+	}
+
+	public ShakeDetection(Context c, float threshold) {
 		mAccel = 0.00f;
 		mAccelCurrent = SensorManager.GRAVITY_EARTH;
 		mAccelLast = SensorManager.GRAVITY_EARTH;
 		mCtx = c;
+		mThreshold = threshold;
 	}
 
+	@Override
 	public void onSensorChanged(SensorEvent se) {
 		float x = se.values[0];
 		float y = se.values[1];
 		float z = se.values[2];
 		mAccelLast = mAccelCurrent;
-		mAccelCurrent = (float) Math.sqrt((double) (x * x + y * y + z * z));
+		mAccelCurrent = (float) Math.sqrt(x * x + y * y + z * z);
 		float delta = mAccelCurrent - mAccelLast;
 		mAccel = mAccel * 0.9f + delta; // perform low-cut filter
 
-		if (mAccel > SHAKE_THRESHOLD) {
+		if (mAccel > mThreshold) {
 			Log.d(TAG, "Shaken Strength: " + mAccel);
 			Intent intent = new Intent();
 			intent.putExtra(SHAKEN, true);
@@ -47,10 +55,11 @@ public class ShakeDetection implements SensorEventListener {
 		}
 	}
 
+	@Override
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
 	}
-	
-	public static String getActionName(){
+
+	public static String getActionName() {
 		return TAG;
 	}
 }
